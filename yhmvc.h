6798e7b27262d2b9e3df2725616a60398a59bea5
trarck <trarck@gmail.com>
2013-12-23 17:52:51 +0800

    加入总的header 文件

---
+++ yhmvc.h
@@ -0,0 +1,20 @@
+﻿#ifndef COCOS_YHMVC_YHMVC_H_
+#define COCOS_YHMVC_YHMVC_H_
+
+#include "YHMVCMacros.h"
+
+#include "Core/Layer.h"
+#include "Core/LayerController.h"
+#include "Core/Scene.h"
+#include "Core/Window.h"
+#include "Core/SceneDirector.h"
+
+#include "Controllers/ControllerDirector.h"
+#include "Controllers/NavigateController.h"
+
+#include "Builders/Builder.h"
+#include "Builders/CCBBuilder.h"
+#include "Builders/YHBuilder.h"
+
+
+#endif //COCOS_YHMVC_YHMVC_H_
