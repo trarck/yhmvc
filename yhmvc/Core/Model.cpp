a80bcaa7563a32834a60114ec9e23037a70f391f
trarck <trarck@gmail.com>
2014-02-12 09:39:16 +0800

    rename modle to model

---
+++ yhmvc/Core/Model.cpp
@@ -0,0 +1,20 @@
+﻿#include "Model.h"
+
+NS_CC_YHMVC_BEGIN
+
+Model::Model()
+{
+
+}
+
+Model::~Model()
+{
+	
+}
+
+bool Model::init()
+{
+    return true;
+}
+
+NS_CC_YHMVC_END
---
+++ yhmvc/Core/Model.h
@@ -0,0 +1,23 @@
+﻿#ifndef COCOS_YHMVC_MODEL_H_
+#define COCOS_YHMVC_MODEL_H_
+
+#include "cocos2d.h"
+#include "../YHMVCMacros.h"
+
+NS_CC_YHMVC_BEGIN
+
+class Model : public CCObject
+{
+public:
+
+	Model();
+
+	~Model();
+    
+	bool init();
+
+};
+
+NS_CC_YHMVC_END
+
+#endif // COCOS_YHMVC_MODEL_H_
