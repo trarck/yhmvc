91bcb352a1ea4023f5725faee6a1f7026d552523
trarck <trarck@gmail.com>
2014-02-12 09:36:32 +0800

    加入modle的定义

---
+++ yhmvc/Core/Modle.cpp
@@ -0,0 +1,20 @@
+﻿#include "Modle.h"
+
+NS_CC_YHMVC_BEGIN
+
+Modle::Modle()
+{
+
+}
+
+Modle::~Modle()
+{
+	
+}
+
+bool Modle::init()
+{
+    return true;
+}
+
+NS_CC_YHMVC_END
---
+++ yhmvc/Core/Modle.h
@@ -0,0 +1,23 @@
+﻿#ifndef COCOS_YHMVC_MODLE_H_
+#define COCOS_YHMVC_MODLE_H_
+
+#include "cocos2d.h"
+#include "../YHMVCMacros.h"
+
+NS_CC_YHMVC_BEGIN
+
+class Modle : public CCObject
+{
+public:
+
+	Modle();
+
+	~Modle();
+    
+	bool init();
+
+};
+
+NS_CC_YHMVC_END
+
+#endif // COCOS_YHMVC_MODLE_H_
